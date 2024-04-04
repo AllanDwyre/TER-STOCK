6d5dcf741b11a822a89d904ccacfa6646167f1d6
denilblecodeur <58654735+denilblecodeur@users.noreply.github.com>
2024-04-04 19:34:36 +0200

    updates page produit

--- front/hive_stock/lib/Product/views/product_page.dart
+++ front/hive_stock/lib/Product/views/product_page.dart
@@ -4,15 +4,20 @@ import '../models/product.dart';
 import 'product_body.dart';
 
 class ProductScreen extends StatelessWidget {
+
   final Product product;
 
   const ProductScreen(this.product, {super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => ProductScreen(products.first));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black, size: 30.0),
