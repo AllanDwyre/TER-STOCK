b5fd15b2b56b1ed1f7cb605b66fe5163c7a2e65e
NinaShine <122785742+NinaShine@users.noreply.github.com>
2024-04-04 19:20:38 +0200

    update authController.js et serveurs

--- back/LOGIN/routes/routes.js
+++ back/LOGIN/routes/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController.js");
+const controllerTest = require("../controller/controllerTest.js");
 
-router.post('/login/Name', authController.loginName);
-router.post('/login/FirstName',authController.loginFirstName);
-router.post('/login/Email',authController.loginEmail);
+router.post('/login/Name', authController.loginUserName);
+//pour le test 
+router.post('/login',controllerTest.loginName);
+
+router.post('/login/Email',authController.signUpEmail);
 router.get('/homePage', authController.home);
 router.post('/signupDate',authController.signupDate);
 router.post('/signupTel', authController.signupTel);
