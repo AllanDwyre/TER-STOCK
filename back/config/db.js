79b8866dab03e7b515a366ab00a439b620e43eb1
DaniVV02 <122785639+DaniVV02@users.noreply.github.com>
2024-05-18 16:46:36 +0200

    Update db.js

--- back/config/db.js
+++ back/config/db.js
@@ -78,160 +78,23 @@ const modelsCloud = initModels(sequelizeCloud, DataTypes);
 }*/
 
 // Synchronisation des données pour chaque modèle
-async function synchronizeAdresse() {
+async function synchronizeTables() {
   try {
     for(const model in modelsCloud ){
-      const tableL = modelsLocale[model];
-      const tableC = modelsCloud[model];
-      const localAdresses = await tableL.findAll();
-      for (const local of localAdresses){
-        const localAdresse = local.dataValues;
-        await tableC.upsert(localAdresse).then( res => {
-          console.log(res);
-        });
+      const tableLocale = modelsLocale[model];
+      const tableCloud = modelsCloud[model];
+      const localModels = await tableLocale.findAll();
+      for (const local of localModels){
+        const localModel = local.dataValues;
+        await tableCloud.upsert(localModel);
       }
-
-      // const localAdresses = await modelsLocale.adresse.findAll();
-      /*for (const local of localAdresses) {
-        const localAdresse = local.dataValues;
-        await modelsCloud.adresse.upsert(localAdresse).then( res => {
-          console.log(res);
-        });
-
-      } */
     }
     console.log("Toutes les données ont été synchronisées avec succès.");
-
-    // Ajoutez d'autres modèles et synchronisations si nécessaire pour d'autres tables
   } catch (error) {
     console.error('Erreur lors de la synchronisation des données :', error);
   }
 }
-synchronizeAdresse();
-
-
-/* Synchronisation des données pour chaque modèle
-async function synchronizeAdresse() {
-    try {
-        // Synchronisation de la table categorie
-        //await modelLocal.sync();
-        const localAdresses = await modelsLocale.adresse.findAll();
-
-      // Pour chaque donnée locale, vérifiez si elle existe déjà dans le cloud
-      for (const local of localAdresses) {
-        const localID = local.dataValues.ADRESSE_ID;
-        const localData = local.dataValues;
-
-        // Vérifiez si la catégorie locale existe déjà dans le cloud
-        const cloudAdresse = await modelsCloud.adresse.findOne({
-            where: {
-                ADRESSE_ID: localID
-            }
-        });
-        for (const key in localData) {
-          
-        // Si la catégorie n'existe pas dans le cloud, insérez-la
-        if (!cloudAdresse) {
-          await modelsCloud.adresse.create(localData);
-          console.log(`Adresse synchronisée : ${localID}, ${localData}`);
-        }else if (localData[key] !== cloudAdresse[key]) {
-          //cloudAdresse.dataValues !== localData
-          await cloudAdresse.update(localData);
-          console.log(`Adresse mise à jour : ${localID}, ${localData}`);
-          //console.log("non");
-      }
-        /*else{
-          console.log("Aucune insertion ou mise à jour nécessaire ");
-        }
-      }
-    }
-
-      console.log("Toutes les données ont été synchronisées avec succès.");
-
-      // Ajoutez d'autres modèles et synchronisations si nécessaire pour d'autres tables
-    } catch (error) {
-      console.error('Erreur lors de la synchronisation des données :', error);
-    }
-  }
-synchronizeAdresse();
-*/
-
-  // Synchronisation des données pour chaque modèle
-async function synchronizeCategorie() {
-  try {
-      // Synchronisation de la table categorie
-      const localCategories = await modelLocal.findAll();
-
-    // Pour chaque donnée locale, vérifiez si elle existe déjà dans le cloud
-    for (const localCategory of localCategories) {
-      const localCategorieID = localCategory.dataValues.CATEGORIE_ID;
-      const localCategorieNom = localCategory.dataValues.NOM_CATEGORIE;
-
-      // Vérifiez si la catégorie locale existe déjà dans le cloud
-      const cloudCategory = await modelCloud.findOne({
-          where: {
-              CATEGORIE_ID: localCategorieID
-          }
-      });
-      // Si la catégorie n'existe pas dans le cloud, insérez-la
-      if (!cloudCategory) {
-        await modelCloud.create({
-            CATEGORIE_ID: localCategorieID,
-            NOM_CATEGORIE: localCategorieNom
-        });
-        console.log(`Catégorie synchronisée : ${localCategorieID}, ${localCategorieNom}`);
-      }
-      else{
-        console.log("Catégorie déjà dans le cloud");
-      }
-    }
-
-    console.log("Toutes les données synchronisées avec succès.");
-
-    // Ajoutez d'autres modèles et synchronisations si nécessaire pour d'autres tables
-  } catch (error) {
-    console.error('Erreur lors de la synchronisation des données :', error);
-  }
-}
-
-// Synchronisation des données pour chaque modèle
-async function synchronizeEmploye() {
-  try {
-      // Synchronisation de la table categorie
-      //await modelLocal.sync();
-      const localCategories = await modelLocal.findAll();
-
-    // Pour chaque donnée locale, vérifiez si elle existe déjà dans le cloud
-    for (const localCategory of localCategories) {
-      const localCategorieID = localCategory.dataValues.CATEGORIE_ID;
-      const localCategorieNom = localCategory.dataValues.NOM_CATEGORIE;
-
-      // Vérifiez si la catégorie locale existe déjà dans le cloud
-      const cloudCategory = await modelCloud.findOne({
-          where: {
-              CATEGORIE_ID: localCategorieID
-          }
-      });
-      // Si la catégorie n'existe pas dans le cloud, insérez-la
-      if (!cloudCategory) {
-        await modelCloud.create({
-            CATEGORIE_ID: localCategorieID,
-            NOM_CATEGORIE: localCategorieNom
-        });
-        console.log(`Catégorie synchronisée : ${localCategorieID}, ${localCategorieNom}`);
-      }
-      else{
-        console.log("Catégorie déjà dans le cloud");
-      }
-    }
-
-    console.log("Toutes les données synchronisées avec succès.");
-
-    // Ajoutez d'autres modèles et synchronisations si nécessaire pour d'autres tables
-  } catch (error) {
-    console.error('Erreur lors de la synchronisation des données :', error);
-  }
-}
+synchronizeTables();
 
 
 // Exporter l'objet Sequelize configuré
