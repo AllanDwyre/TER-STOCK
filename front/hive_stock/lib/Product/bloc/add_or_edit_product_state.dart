e74b4f8e6b8d00ab655f4d8f50ba9a7be8890291
AllanDwyre <allan.dwyre@gmail.com>
2024-05-27 16:13:35 +0200

    Feature : Profile implémentation (basics)

---
+++ front/hive_stock/lib/Product/bloc/add_or_edit_product_state.dart
@@ -0,0 +1,6 @@
+part of 'add_or_edit_product_bloc.dart';
+
+final class AddOrEditProductState extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
--- front/hive_stock/lib/user/views/profile.dart
+++ front/hive_stock/lib/user/views/profile.dart
@@ -3,7 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_stock/authentication/bloc/authentication_bloc.dart';
 import 'package:hive_stock/user/bloc/user_bloc.dart';
 import 'package:hive_stock/user/repository/user_repository.dart';
-import 'package:hive_stock/utils/widgets/custom_appbar.dart';
+import 'package:hive_stock/utils/app/bridge_repository.dart';
+import 'package:hive_stock/utils/constants/constants.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -20,8 +21,17 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: customAppBar(context),
-      body: const ProfileBody(),
+      appBar: AppBar(),
+      body: RepositoryProvider(
+        create: (context) => UserRepository(
+            bridge: RepositoryProvider.of<BridgeRepository>(context)),
+        child: BlocProvider(
+          create: (context) => UserBloc(
+            userRepository: RepositoryProvider.of<UserRepository>(context),
+          )..add(OnUserFetched()),
+          child: const ProfileBody(),
+        ),
+      ),
     );
   }
 }
@@ -35,42 +45,65 @@ class ProfileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     // ColorScheme colorTheme = Theme.of(context).colorScheme;
     TextTheme textTheme = Theme.of(context).textTheme;
-    Size size = MediaQuery.of(context).size;
+    // Size size = MediaQuery.of(context).size;
 
     return BlocBuilder<UserBloc, UserState>(
       builder: (context, state) {
-        return Container(
-          width: size.width,
-          height: size.height,
-          color: const Color.fromARGB(206, 205, 205, 205),
-          child: Padding(
-            padding: EdgeInsets.only(top: size.height / 6),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "UserID: ${state.user?.id ?? "-"}",
-                  style: textTheme.titleLarge?.copyWith(color: Colors.black),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  state.user?.fullname ?? "-",
-                  style: textTheme.titleLarge?.copyWith(color: Colors.black),
-                ),
-                Text(
-                  state.user?.username ?? "-",
-                  style: textTheme.bodyLarge?.copyWith(color: Colors.black),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  child: const Text('Logout'),
-                  onPressed: () {
-                    context
-                        .read<AuthenticationBloc>()
-                        .add(AuthenticationLogoutRequested());
-                  },
-                ),
-              ],
-            ),
+        return Padding(
+          padding: defaultPagePadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //image
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black12,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      Text(
+                        state.user?.fullname ?? "-",
+                        style:
+                            textTheme.titleLarge?.copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        state.user?.username ?? "-",
+                        style:
+                            textTheme.bodyLarge?.copyWith(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "UserID: ${state.user?.id ?? "-"}",
+                style: textTheme.bodyMedium?.copyWith(color: Colors.black),
+              ),
+              Text(
+                "Birthday: ${state.user?.birthday ?? "-"}",
+                style: textTheme.bodyMedium?.copyWith(color: Colors.black),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                child: const Text('Logout'),
+                onPressed: () {
+                  context
+                      .read<AuthenticationBloc>()
+                      .add(AuthenticationLogoutRequested());
+                },
+              ),
+            ],
           ),
         );
       },
