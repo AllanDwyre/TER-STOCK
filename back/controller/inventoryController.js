b010a03425cf5eb5239fcd0d945989184b0255db
“NinaShine” <salhinina2002@gmail.com>
2024-05-29 15:40:24 +0200

    maj

--- back/controller/inventoryController.js
+++ back/controller/inventoryController.js
@@ -30,47 +30,7 @@ module.exports = {
           });
     } 
   },
-
   getTopSellingProduct: async (req, res) => {
-    try {
-      console.log(`Top Selling Product =>`);
-      await models.produit
-        .findAll({
-          include: [
-            {
-              model: models.produit_vendu,
-              as: "produit_vendus",
-              attributes: [],
-              required: true,
-            },
-          ],
-          attributes: ['NOM'],
-          order: [[sequelize.col('produit_vendus.QUANTITE'), "DESC"]],
-        })
-        .then((result) => {
-          if (result.length > 0) {
-            const topProduct = result[0].dataValues;
-            console.table([topProduct]); 
-            res.status(200).json(topProduct);
-          } else {
-            res.status(404).json({ message: "No products found" });
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching top selling product:", error);
-          res.status(500).json({
-            message: "Error fetching top selling product: " + error.message,
-          });
-        });
-    } catch (error) {
-      res.status(500).json({
-        message:
-          "Erreur lors de la récupération du top selling product: " + error.message,
-      });
-    }
-  },
-  
-  getTopSellingProduct2: async (req, res) => {
     // On vérifie d'abord si l'en-tête Authorization est présente dans la requête
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
